177af2c571c245c787e8f0c0e1b3961a36c6bcfd
Chris Giblin <cgi@zurich.ibm.com>
2022-04-13 16:12:46 +0200

    Add javadoc
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/main/java/io/strimzi/kafka/topicenc/EncModControl.java
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/EncModControl.java
@@ -4,13 +4,25 @@
  */
 package io.strimzi.kafka.topicenc;
 
+/**
+ * This defines the interface to the Encryption Module to functions
+ * controlling its internal state. So, for example, can an implementation
+ * receiving events from a key management system (KMS), notify the module
+ * to purge a key because it has expired. If we consider the
+ * Encryption Module's encrypt() and decrypt() functions to comprise
+ * the data path, this interface describes its control path. 
+ * 
+ * Currently this interface is a placeholder but will be continually 
+ * extended as the implementation matures.
+ */
 public interface EncModControl {
 
-	//void disableTopic(String topic);
-	
-	//void enableTopic(String topic);
-	
-	//void clearTopic(String topic);
-	
+    /**
+     * Purge the key, indicated by the keyRef argument, from any 
+     * internal state such that the key in question is now longer used.
+     * This supports key revokation.
+     * 
+     * @param keyref A key reference, understood by the Encryption Module and its KMS, identifying the key to purge.
+     */
 	void purgeKey(String keyref);
 }
