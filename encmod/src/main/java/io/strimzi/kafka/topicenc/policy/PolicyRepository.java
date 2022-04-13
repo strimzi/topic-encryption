14b3b5da15da96e8f8381c92966754e1e43d176e
Chris Giblin <cgi@zurich.ibm.com>
2022-04-13 16:14:34 +0200

    Add javadoc comments
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/main/java/io/strimzi/kafka/topicenc/policy/PolicyRepository.java
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/policy/PolicyRepository.java
@@ -4,6 +4,17 @@
  */
 package io.strimzi.kafka.topicenc.policy;
 
+/**
+ * This is the interface to an implementation of a
+ * policy repository containing information on
+ * topics to encrypt. Such a repository can
+ * take many forms such as a JSON file, a REST service,
+ * a database, etc. The encryption module uses this interface
+ * to retrieve policy information without being concerned with 
+ * the implementation details on repository housing policy.
+ * A trivial implementation of this interface is TestPolicyRepo
+ * used for testing.
+ */
 public interface PolicyRepository {
 	
 	TopicPolicy getTopicPolicy(String topicName);
