6dfb13c1ccf75837b39772b5240311831c3f0d0e
Chris Giblin <cgi@zurich.ibm.com>
2022-07-06 08:35:34 +0200

    add exceptions to getKey()
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/main/java/io/strimzi/kafka/topicenc/kms/KeyMgtSystem.java
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/kms/KeyMgtSystem.java
@@ -1,14 +1,18 @@
 /*
- * Copyright Strimzi authors.
- * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ * Copyright Strimzi authors. License: Apache License 2.0 (see the file LICENSE or
+ * http://apache.org/licenses/LICENSE-2.0.html).
  */
 package io.strimzi.kafka.topicenc.kms;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.SecretKey;
 
 public interface KeyMgtSystem {
 
-    void setCredential(String cred);
- 
-    SecretKey getKey(String keyReference);
+    SecretKey getKey(String keyReference) throws URISyntaxException, IOException,
+            InterruptedException, InvalidKeySpecException, NoSuchAlgorithmException;
 }
