dd04253ab7a167f350bd5ae04bc20653d4b6b0bc
Chris Giblin <cgi@zurich.ibm.com>
2022-07-06 08:46:37 +0200

    topic now contains ref to kms instance, not its def (config info). Add validate method. Formatting
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/main/java/io/strimzi/kafka/topicenc/policy/TopicPolicy.java
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/policy/TopicPolicy.java
@@ -4,42 +4,87 @@
  */
 package io.strimzi.kafka.topicenc.policy;
 
+import io.strimzi.kafka.topicenc.common.Strings;
+import io.strimzi.kafka.topicenc.kms.KeyMgtSystem;
+
 public class TopicPolicy {
 
-	private String topic;
-	private String encMethod;
-	private String keyReference;
-	private KmsDefinition kms;
-	private String credential;
-	
-	public String getTopic() {
-		return topic;
-	}
-	public void setTopic(String topic) {
-		this.topic = topic;
-	}
-	public String getEncMethod() {
-		return encMethod;
-	}
-	public void setEncMethod(String encMethod) {
-		this.encMethod = encMethod;
-	}
-	public String getKeyReference() {
-		return keyReference;
-	}
-	public void setKeyReference(String keyReference) {
-		this.keyReference = keyReference;
-	}
-	public KmsDefinition getKms() {
-		return kms;
-	}
-	public void setKms(KmsDefinition kms) {
-		this.kms = kms;
-	}
-	public String getCredential() {
-		return credential;
-	}
-	public void setCredential(String credential) {
-		this.credential = credential;
-	}
+    private String topic;
+    private String encMethod;
+    private String keyReference;
+    private KeyMgtSystem kms;
+    private String kmsName;
+    private String credential;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public TopicPolicy setTopic(String topic) {
+        this.topic = topic;
+        return this;
+    }
+
+    public String getEncMethod() {
+        return encMethod;
+    }
+
+    public TopicPolicy setEncMethod(String encMethod) {
+        this.encMethod = encMethod;
+        return this;
+    }
+
+    public String getKeyReference() {
+        return keyReference;
+    }
+
+    public TopicPolicy setKeyReference(String keyReference) {
+        this.keyReference = keyReference;
+        return this;
+    }
+
+    public KeyMgtSystem getKms() {
+        return kms;
+    }
+
+    public TopicPolicy setKms(KeyMgtSystem kms) {
+        this.kms = kms;
+        return this;
+    }
+
+    public String getKmsName() {
+        return kmsName;
+    }
+
+    public TopicPolicy setKmsName(String kmsName) {
+        this.kmsName = kmsName;
+        return this;
+    }
+
+    public String getCredential() {
+        return credential;
+    }
+
+    public TopicPolicy setCredential(String credential) {
+        this.credential = credential;
+        return this;
+    }
+
+    public void validate() {
+        if (Strings.isNullOrEmpty(getTopic())) {
+            throw new IllegalArgumentException("Topic name missing from policy definition");
+        }
+        if (Strings.isNullOrEmpty(getKmsName())) {
+            String msg = String.format(
+                    "Policy for topic %s does not contain required KMS reference.",
+                    getTopic());
+            throw new IllegalArgumentException(msg);
+        }
+        if (Strings.isNullOrEmpty(getKeyReference())) {
+            String msg = String.format(
+                    "Policy for topic %s does not contain a key reference.",
+                    getTopic());
+            throw new IllegalArgumentException(msg);
+        }
+    }
 }
