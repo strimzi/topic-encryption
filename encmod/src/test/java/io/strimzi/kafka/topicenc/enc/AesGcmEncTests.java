a7af275f5ec75ee6b7119159ce7c6b33bfdc0b43
Chris Giblin <cgi@zurich.ibm.com>
2022-07-06 09:00:08 +0200

    formatting
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/test/java/io/strimzi/kafka/topicenc/enc/AesGcmEncTests.java
+++ encmod/src/test/java/io/strimzi/kafka/topicenc/enc/AesGcmEncTests.java
@@ -21,12 +21,12 @@ import io.strimzi.kafka.topicenc.ser.AesGcmV1SerDer;
 import io.strimzi.kafka.topicenc.ser.EncSerDerException;
 
 public class AesGcmEncTests {
-    
+
     private static final String TEST_MSG = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-=+[]{}";
 
     TestKms kms;
     AesGcmEncrypter enc;
-    
+
     @Before
     public void testsInit() throws NoSuchAlgorithmException {
         kms = new TestKms();
@@ -41,7 +41,7 @@ public class AesGcmEncTests {
     public void basicTestAesGcm() {
 
         byte[] testMsg = TEST_MSG.getBytes(StandardCharsets.UTF_8);
-        
+
         EncData encData;
         try {
             encData = enc.encrypt(testMsg);
@@ -52,32 +52,32 @@ public class AesGcmEncTests {
         try {
             byte[] plaintext = enc.decrypt(encData);
             Assert.assertArrayEquals(testMsg, plaintext);
-            
+
             String plaintextStr = new String(plaintext, "UTF-8");
             Assert.assertEquals(TEST_MSG, plaintextStr);
-            
+
         } catch (Exception e) {
             fail("Error deencrypting test message: " + e.toString());
         }
     }
-    
+
     /**
      * Basic test of serialization, deserialization of encrypted data.
      */
     @Test
     public void basicTestSerDer() {
         byte[] testMsg = TEST_MSG.getBytes(StandardCharsets.UTF_8);
-        testSerDer(testMsg);        
+        testSerDer(testMsg);
     }
-    
+
     /**
-     * Tests serialization, deserialization of encrypted data
-     * using buffers of random size and data.
+     * Tests serialization, deserialization of encrypted data using buffers of
+     * random size and data.
      */
     @Test
     public void randomTestSerDer() {
         int numTests = 200;
-        
+
         // tiny
         testRandomSerDer(1, 2, numTests);
         // small
@@ -89,11 +89,11 @@ public class AesGcmEncTests {
         // extra large
         testRandomSerDer(1000000, 5000000, numTests);
     }
-    
+
     private void testRandomSerDer(int minBufSize, int maxBufSize, int iterations) {
         Random rand = new Random();
-        
-        for (int i=0; i< iterations; i++) {
+
+        for (int i = 0; i < iterations; i++) {
             int bufSize = rand.nextInt(maxBufSize - minBufSize) + minBufSize;
             byte[] buf = CryptoUtils.createRandom(bufSize);
 
@@ -102,7 +102,7 @@ public class AesGcmEncTests {
     }
 
     private void testSerDer(byte[] testMsg) {
-        
+
         // encrypt test message
         EncData encData;
         try {
@@ -111,7 +111,7 @@ public class AesGcmEncTests {
             fail("Error encrypting test message: " + e.toString());
             return;
         }
-        
+
         // serialize encrypted message
         AesGcmV1SerDer serder = new AesGcmV1SerDer();
         byte[] serialized;
@@ -121,7 +121,7 @@ public class AesGcmEncTests {
             fail("Error serializing encrypted test message: " + e.toString());
             return;
         }
-        
+
         // deserialize serialized buf
         EncData deserialized;
         try {
@@ -130,7 +130,7 @@ public class AesGcmEncTests {
             fail("Error deserializing serialized test message: " + e.toString());
             return;
         }
-        
+
         // decrypt deserialized data, assert plaintext equals original message.
         try {
             byte[] decrypted = enc.decrypt(deserialized);
