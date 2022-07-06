40620fbb79df0f01598ba9f3e7be859a0a4f94cc
Chris Giblin <cgi@zurich.ibm.com>
2022-07-06 08:39:44 +0200

    Add KmsException to framework
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

---
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/kms/KmsException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.kafka.topicenc.kms;
+
+public class KmsException extends Exception {
+
+    private static final long serialVersionUID = -1957947377477505516L;
+
+    public KmsException(String msg) {
+        super(msg);
+    }
+
+    public KmsException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
