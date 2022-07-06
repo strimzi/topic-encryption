0344d2ee33e92821eaf657062a6573df811eea7f
Chris Giblin <cgi@zurich.ibm.com>
2022-07-06 08:31:18 +0200

    formatting
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/main/java/io/strimzi/kafka/topicenc/enc/CryptoUtils.java
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/enc/CryptoUtils.java
@@ -8,10 +8,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.util.Base64;
 import java.util.Map;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,44 +24,55 @@ import org.slf4j.LoggerFactory;
 public class CryptoUtils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CryptoUtils.class);
-    
-    public static final String AES = "AES";
-	public static final String AES_GCM_PADDING = AES + "/GCM/PKCS5Padding";
-	public static final String AES256_GCM_NOPADDING = "AES_256/GCM/NoPadding";
 
-	/**
-	 * Create an array of bytes with random bits, suitable for use
-	 * as nonce or initialization vector.
-	 *
-	 * @param sizeBytes
-	 * @return
-	 */
-	public static byte[] createRandom(int numBytes) {
+    public static final String AES = "AES";
+    public static final String AES_GCM_PADDING = AES + "/GCM/PKCS5Padding";
+    public static final String AES256_GCM_NOPADDING = "AES_256/GCM/NoPadding";
+
+    /**
+     * Create an array of bytes with random bits, suitable for use as nonce or
+     * initialization vector.
+     *
+     * @param sizeBytes
+     * @return
+     */
+    public static byte[] createRandom(int numBytes) {
         byte[] buf = new byte[numBytes];
         new SecureRandom().nextBytes(buf);
         return buf;
-	}
-	
-	public static SecretKey generateKey(String algo, int keySize) throws NoSuchAlgorithmException  {
-	    KeyGenerator kgen = KeyGenerator.getInstance(algo);
-	    kgen.init(keySize);
-	    return kgen.generateKey();		
-	}
+    }
 
-	public static SecretKey generateAesKey(int keySize) throws NoSuchAlgorithmException  {
-		return generateKey(AES, keySize);
-	}
-	
-	public static void logCiphers() {
-	    for (Provider provider : Security.getProviders()) {
-	        LOGGER.debug("Cipher provider: {}", provider.getName());
-	        for (Map.Entry<Object, Object> entry : provider.entrySet()) {
-	            if (((String) entry.getValue()).contains("GCM")) {
-	                LOGGER.debug("key: [%s]  value: [%s]%n",
-	                    entry.getKey(),
-	                    entry.getValue());
-	            }
-	        }
-	    }	    
-	}
+    public static SecretKey generateKey(String algo, int keySize) throws NoSuchAlgorithmException {
+        KeyGenerator kgen = KeyGenerator.getInstance(algo);
+        kgen.init(keySize);
+        return kgen.generateKey();
+    }
+
+    public static SecretKey generateAesKey(int keySize) throws NoSuchAlgorithmException {
+        return generateKey(AES, keySize);
+    }
+
+    public static String base64Encode(SecretKey key) {
+        byte[] keyBuf = key.getEncoded();
+        return Base64.getEncoder().encodeToString(keyBuf);
+    }
+
+    public static SecretKey base64Decode(String key64) {
+        byte[] decodedKey = Base64.getDecoder().decode(key64);
+        // we assume AES
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, AES);
+    }
+
+    public static void logCiphers() {
+        for (Provider provider : Security.getProviders()) {
+            LOGGER.debug("Cipher provider: {}", provider.getName());
+            for (Map.Entry<Object, Object> entry : provider.entrySet()) {
+                if (((String) entry.getValue()).contains("GCM")) {
+                    LOGGER.debug("key: [%s]  value: [%s]%n",
+                            entry.getKey(),
+                            entry.getValue());
+                }
+            }
+        }
+    }
 }
