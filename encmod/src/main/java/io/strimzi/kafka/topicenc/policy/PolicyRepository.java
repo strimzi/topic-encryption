cb20424665f678e3d4300659cb5627f673f79cff
Chris Giblin <cgi@zurich.ibm.com>
2022-07-06 08:48:43 +0200

    Refactor such that a policy repo returns topic policy only, not kms config info
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- encmod/src/main/java/io/strimzi/kafka/topicenc/policy/PolicyRepository.java
+++ encmod/src/main/java/io/strimzi/kafka/topicenc/policy/PolicyRepository.java
@@ -5,19 +5,14 @@
 package io.strimzi.kafka.topicenc.policy;
 
 /**
- * This is the interface to an implementation of a
- * policy repository containing information on
- * topics to encrypt. Such a repository can
- * take many forms such as a JSON file, a REST service,
- * a database, etc. The encryption module uses this interface
- * to retrieve policy information without being concerned with 
- * the implementation details on repository housing policy.
- * A trivial implementation of this interface is TestPolicyRepo
- * used for testing.
+ * This is the interface to an implementation of a policy repository containing
+ * information on topics to encrypt. Such a repository can take many forms such
+ * as a JSON file, a REST service, a database, etc. The encryption module uses
+ * this interface to retrieve policy information without being concerned with
+ * the implementation details on repository housing policy. A trivial
+ * implementation of this interface is TestPolicyRepo used for testing.
  */
 public interface PolicyRepository {
-	
-	TopicPolicy getTopicPolicy(String topicName);
-	
-	KmsDefinition getKmsDefinition(String kmsName);
+
+    TopicPolicy getTopicPolicy(String topicName);
 }
