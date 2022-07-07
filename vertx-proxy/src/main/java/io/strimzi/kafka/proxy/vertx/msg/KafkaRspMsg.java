bc57cae79e78d3ac07cb6400700ae92a040544ae
Chris Giblin <cgi@zurich.ibm.com>
2022-07-07 11:54:24 +0200

    place msg classes into msg package
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

---
+++ vertx-proxy/src/main/java/io/strimzi/kafka/proxy/vertx/msg/KafkaRspMsg.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.kafka.proxy.vertx.msg;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import org.apache.kafka.common.requests.ResponseHeader;
+
+import io.vertx.core.buffer.Buffer;
+
+public class KafkaRspMsg extends AbstractKafkaMsg {
+	
+	private final static int FIXED_HEADER_LEN = 4;
+
+	private ResponseHeader header;
+	private byte[] headerBytes;
+	private final short version;
+	
+	public KafkaRspMsg(Buffer rawMsg, short version) {
+	    super(rawMsg);
+	    this.version = version;
+	}
+	
+	public short getVersion() {
+	    return version;
+	}
+	
+	public ResponseHeader getHeader() {
+	    if (header == null) {
+	        header = ResponseHeader.parse(getPayload(), version);
+	    }
+		return header;
+	}
+	
+	public byte[] getHeaderBytes() {
+	    
+		if (headerBytes == null) {
+			// to do: test, not correct
+	    	int headerSize = FIXED_HEADER_LEN; 
+	    	int destIndex = MSG_SIZE_LEN + headerSize + 1;
+			headerBytes = Arrays.copyOfRange(rawMsg.getBytes(),
+					                         MSG_SIZE_LEN,
+					                         destIndex);
+		}
+		return headerBytes;
+	}
+	
+	@Override
+    protected ByteBuffer extractKafkaPayload(Buffer kafkaMsg) {
+        // copy bytes after leading 4 bytes containing message len:
+	    int msgLen = kafkaMsg.length();
+	    int headerLen = MSG_SIZE_LEN; // + FIXED_HEADER_LEN;
+	    return ByteBuffer.wrap(kafkaMsg.getBytes(headerLen, msgLen));
+        //byte[] dataBytes = Arrays.copyOfRange(kafkaMsg.getBytes(), 0, kafkaMsg.getBytes().length);
+        //return ByteBuffer.wrap(dataBytes);
+    }
+	
+}
