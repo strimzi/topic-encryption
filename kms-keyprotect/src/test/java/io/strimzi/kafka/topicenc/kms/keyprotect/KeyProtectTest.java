0d167b606f21d0784d922a8aaacfc525fc731391
Chris Giblin <cgi@zurich.ibm.com>
2022-09-06 11:30:23 +0200

    Update test to use service loader, load test params and data from external files
    Signed-off-by: Chris Giblin <cgi@zurich.ibm.com>

--- kms-keyprotect/src/test/java/io/strimzi/kafka/topicenc/kms/keyprotect/KeyProtectTest.java
+++ kms-keyprotect/src/test/java/io/strimzi/kafka/topicenc/kms/keyprotect/KeyProtectTest.java
@@ -1,51 +1,68 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
 package io.strimzi.kafka.topicenc.kms.keyprotect;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-import java.net.URI;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import javax.crypto.SecretKey;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.strimzi.kafka.topicenc.kms.KeyMgtSystem;
 import io.strimzi.kafka.topicenc.kms.KmsDefinition;
 import io.strimzi.kafka.topicenc.kms.KmsException;
+import io.strimzi.kafka.topicenc.kms.KmsFactoryManager;
 
-public class KeyProtectTests {
+/**
+ * Units tests for the Key Protection KMS implementation.
+ */
+public class KeyProtectTest {
 
-    private static final String KP_URI = "https://us-south.kms.cloud.ibm.com";
-    private static final String KEY_REF = "";
-    private static final String KP_INSTANCEID = "";
+    private static final String NAME = "name";
+    private static final String KP_URI = "uri";
+    private static final String KEY_REF = "key_ref";
+    private static final String KP_INSTANCEID = "instance_id";
+    private static final String KP_CREDENTIAL = "credential";
+    private static final String KP_PROPS_FILE = "src/test/resources/keyprotect.properties";
+    private static final String KMS_DEF_FILE = "src/test/resources/kmsdef.json";
 
     KeyMgtSystem keyProtect;
     KmsDefinition kmsDef;
+    String testKeyRef;
 
     @Before
     public void setUp() {
-        // create the KMS:
         try {
-            kmsDef = new KmsDefinition()
-                    .setUri(new URI(KP_URI))
-                    .setCredential("")
-                    .setInstanceId(KP_INSTANCEID);
+            testKeyRef = loadTestKeyRef(new File(KP_PROPS_FILE));
+            kmsDef = loadKmsDef(new File(KMS_DEF_FILE));
 
-            keyProtect = new KeyProtectKms(kmsDef);
+            keyProtect = KmsFactoryManager.getInstance().createKms(kmsDef);
 
         } catch (Exception e) {
-            fail("Error creating kms: " + e.toString());
+            fail("Error setting up key-protect test: " + e.toString());
             return;
         }
     }
 
     @Test
     public void basicTests() {
-
         // retrieve test key
         try {
-            SecretKey key = keyProtect.getKey(KEY_REF);
+            SecretKey key = keyProtect.getKey(testKeyRef);
             assertNotNull("Retrieved key is null.", key);
         } catch (KmsException e) {
             fail("Error retrieving key from kms: " + e.toString());
@@ -53,4 +70,37 @@ public class KeyProtectTests {
         }
     }
 
+    /**
+     * Load KMS def from a JSON file.
+     * 
+     * @param file
+     * @return
+     * @throws StreamReadException
+     * @throws DatabindException
+     * @throws IOException
+     */
+    private KmsDefinition loadKmsDef(File file)
+            throws StreamReadException, DatabindException, IOException {
+        ObjectMapper objMapper = new ObjectMapper();
+        KmsDefinition kmsDef = objMapper.readValue(file,
+                new TypeReference<KmsDefinition>() {
+                });
+        kmsDef.validate();
+        return kmsDef;
+    }
+
+    /**
+     * Load the key ref to use during testing.
+     * 
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    private String loadTestKeyRef(File file) throws IOException {
+        Properties props = new Properties();
+        try (FileInputStream in = new FileInputStream(file)) {
+            props.load(in);
+        }
+        return props.getProperty(KEY_REF);
+    }
 }
